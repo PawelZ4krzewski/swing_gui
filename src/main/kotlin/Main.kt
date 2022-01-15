1a76ed241e9be7db1b8e2bdeb5db5df31c5e7c3f
PawelZ4krzewski <61842320+PawelZ4krzewski@users.noreply.github.com>
2022-01-15 01:33:18 +0100

    add question page

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,4 @@
-import java.awt.BorderLayout
-import java.awt.CardLayout
-import java.awt.Color
-import java.awt.Dimension
+import java.awt.*
 import javax.swing.*
 import javax.swing.BorderFactory.createEmptyBorder
 
@@ -28,6 +25,39 @@ fun connectScreen(){
     val createRoomButton = JButton("Create Room")
     createRoomButton.setBounds(40, 110, 200, 40)
 
+    //Room panel
+    val nrQuetionLabel = JLabel("Question 1",SwingConstants.CENTER)
+    nrQuetionLabel.setBounds(50,0,300,50)
+    nrQuetionLabel.font = Font("Serif", Font.BOLD, 25)
+
+    val questionLabel = JLabel("<html>Pytanie na śniadanie XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDddd<html>",SwingConstants.CENTER)
+    questionLabel.setBounds(50,40,300,150)
+    questionLabel.font = Font("Serif", Font.BOLD, 25)
+
+    val answerGroup = JPanel(GridLayout(2,2,10,10))
+    answerGroup.setBounds(45,250,300,300)
+    answerGroup.background = Color.BLACK
+
+    val answer1 = JButton("A")
+    answer1.background = Color.RED
+    answer1.setSize(Dimension(75, 75))
+    answer1.font = Font("Serif", Font.BOLD, 50)
+
+    val answer2 = JButton("B")
+    answer2.background = Color.GREEN
+    answer2.setSize(Dimension(75, 75))
+    answer2.font = Font("Serif", Font.BOLD, 50)
+
+    val answer3 = JButton("C")
+    answer3.background = Color.YELLOW
+    answer3.setSize(Dimension(75, 75))
+    answer3.font = Font("Serif", Font.BOLD, 50)
+
+    val answer4 = JButton("D")
+    answer4.background = Color.BLUE
+    answer4.setSize(Dimension(75, 75))
+    answer4.font = Font("Serif", Font.BOLD, 50)
+
 
     val frame = JFrame("Hello, Kotlin/Swing")
     frame.layout = null
@@ -53,7 +83,17 @@ fun connectScreen(){
     hubPanel.add(connectToRoomButton)
     hubPanel.add(createRoomButton)
 
+    //Room Panel
+    val roomPanel = JPanel(null)
+    roomPanel.background = Color.MAGENTA
 
+    roomPanel.add(nrQuetionLabel)
+    roomPanel.add(questionLabel)
+    roomPanel.add(answerGroup)
+    answerGroup.add(answer1)
+    answerGroup.add(answer2)
+    answerGroup.add(answer3)
+    answerGroup.add(answer4)
 
     frame.setContentPane(connectionPanel)
 
@@ -69,8 +109,18 @@ fun connectScreen(){
 //        crd.next(mainPanel)
     }
 
+    connectToRoomButton.addActionListener({
+        frame.contentPane.remove(hubPanel)
+        frame.repaint()
+
+        frame.setContentPane(roomPanel)
+        frame.invalidate()
+        frame.validate()
+        frame.repaint()
+    })
+
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
-    frame.setSize(Dimension(300, 200))
+    frame.setSize(Dimension(400, 600))
     frame.setLocationRelativeTo(null)
 
 
