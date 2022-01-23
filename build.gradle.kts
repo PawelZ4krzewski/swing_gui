c0b229d99bd0a71217c466a177af405f3f32354f
Wojtek <wojciechkrystyniak@gmail.com>
2022-01-23 10:54:28 +0100

    Generate jar on build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+//    id("com.github.johnrengelman.shadow") version "7.1.2"
     application
+
 }
 
 group = "me.pawel"
@@ -28,4 +30,20 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.withType<Jar> {
+    exclude("META-INF/*.SF", "META-INF/*.DSA", "META-INF/*.RSA", "META-INF/*.MF")
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
 }
